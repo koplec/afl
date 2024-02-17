0e67ae62da710196757e3fb9974e02a2cfa021ac
koplec <kopec@github.com>
2024-02-17 16:12:05 +0900

    UserResourceControllerにcreateResourceのテスト追加

--- packages/backend/src/interfaces/controllers/UserResourceController.test.ts
+++ packages/backend/src/interfaces/controllers/UserResourceController.test.ts
@@ -1,40 +1,64 @@
 import UserResourceController from './UserResourceController';
 import GetUserResource from '../../application/userResource/GetUserResource';
-import {jest, describe, it, expect} from "@jest/globals";
+import {jest, describe, it, expect, beforeEach} from "@jest/globals";
 import { UserResourceRepository } from '../../infrastructure/db/UserResourceRepository';
 import { UserResourceType } from '../../domain/types';
 import { UpdateUserResource, UpdateUserResourceArgsType } from '../../application/userResource/UpdateUserResource';
+import { CreateUserResource, CreateUserResourceArgs } from '../../application/userResource/CreateUserResource';
 
 describe("UserResourceController", () => {
+
+    let userResourceController: UserResourceController;
+    beforeEach(() => {
+        jest.clearAllMocks();
+        const mockUserResourceRepository = jest.genMockFromModule<UserResourceRepository>('../../infrastructure/db/UserResourceRepository');
+        const MockGetUserResource = GetUserResource as jest.MockedClass<typeof GetUserResource>;
+        const getUserResource = new MockGetUserResource(mockUserResourceRepository);
+        getUserResource.execute = jest.fn<(userId: number, resourceId: number) => Promise<UserResourceType|null>>(
+            (userId: number, resourceId: number) => {
+                if(userId === 10 && resourceId === 34){
+                    return Promise.resolve({
+                        id: 10,
+                        userId: 34,
+                        protocolType: "WEBDAV",
+                        url: "http://hogehoge-webdav:port9999",
+                        username: "CCC",
+                        password: "DDD",
+                        directory: "/path/to/directory/EEE"
+                    });
+                }else{
+                    return Promise.resolve(null);
+                }
+            }
+        );
+
+        const MockUpdateUserResource = UpdateUserResource as jest.MockedClass<typeof UpdateUserResource>;
+        const updateUserResource = new MockUpdateUserResource(mockUserResourceRepository);
+        updateUserResource.execute = jest.fn<(args:UpdateUserResourceArgsType) => Promise<boolean>>(
+            (args:UpdateUserResourceArgsType) => {
+                if(args.userId === 1 && args.resourceId === 1){
+                    return Promise.resolve(true);
+                }else{
+                    return Promise.resolve(false);
+                }
+            }
+        );
+
+        const MockCreateUserResource = CreateUserResource as jest.MockedClass<typeof CreateUserResource>;
+        const createUserResource = new MockCreateUserResource(mockUserResourceRepository);
+        createUserResource.execute= jest.fn<(args:CreateUserResourceArgs) => Promise<number>>(
+            (args:CreateUserResourceArgs) => {
+                if(args.type !== "WEBDAV"){
+                    return Promise.reject(new Error("User resource creation failed"));
+                }
+                return Promise.resolve(123);
+                
+            }
+        );
+        userResourceController = new UserResourceController(getUserResource, updateUserResource, createUserResource);
+    });
     describe("getResource", () => {
         it('should return 404 when resource not found and 200 when resource', async () => {
-            const mockUserResourceRepository = jest.genMockFromModule<UserResourceRepository>('../../infrastructure/db/UserResourceRepository');
-            const mockGetUserResource = GetUserResource as jest.MockedClass<typeof GetUserResource>;
-            const getUserResource = new mockGetUserResource(mockUserResourceRepository);
-            getUserResource.execute = jest.fn<(userId: number, resourceId: number) => Promise<UserResourceType|null>>(
-                (userId: number, resourceId: number) => {
-                    if(userId === 10 && resourceId === 34){
-                        return Promise.resolve({
-                            id: 10,
-                            userId: 34,
-                            protocolType: "WEBDAV",
-                            url: "http://hogehoge-webdav:port9999",
-                            username: "CCC",
-                            password: "DDD",
-                            directory: "/path/to/directory/EEE"
-                        });
-                    }else{
-                        return Promise.resolve(null);
-                    }
-                }
-            );
-
-            const MockUpdateUserResource = UpdateUserResource as jest.MockedClass<typeof UpdateUserResource>;
-            const updateUserResource = new MockUpdateUserResource(mockUserResourceRepository);
-
-            const userResourceController = new UserResourceController(
-                getUserResource, updateUserResource
-            );
             const req = { params: { userId: 1, resourceId: 1 } };
             const res = {
                 status: jest.fn().mockReturnThis(),
@@ -66,24 +90,7 @@ describe("UserResourceController", () => {
     });
 
     describe("updateResource", () => {
-        
         it('should return 200 when resource is updated successfully', async () => {
-            const mockUserResourceRepository = jest.genMockFromModule<UserResourceRepository>('../../infrastructure/db/UserResourceRepository');
-            const mockUpdateUserResource = UpdateUserResource as jest.MockedClass<typeof UpdateUserResource>;
-            const updateUserResource = new mockUpdateUserResource(mockUserResourceRepository);
-            updateUserResource.execute = jest.fn<(args:UpdateUserResourceArgsType) => Promise<boolean>>(
-                (args:UpdateUserResourceArgsType) => {
-                    if(args.userId === 1 && args.resourceId === 1){
-                        return Promise.resolve(true);
-                    }else{
-                        return Promise.resolve(false);
-                    }
-                }
-            );
-
-            const userResourceController = new UserResourceController(
-                {} as GetUserResource, updateUserResource
-            );
             const req = { params: { userId: 1, resourceId: 1 }, body: { 
                 url: "http://example.com:12345",
                 type: "WEBDAV",
@@ -102,22 +109,6 @@ describe("UserResourceController", () => {
         });
 
         it('should return 404 when resource is not found', async () => {
-            const mockUserResourceRepository = jest.genMockFromModule<UserResourceRepository>('../../infrastructure/db/UserResourceRepository');
-            const mockUpdateUserResource = UpdateUserResource as jest.MockedClass<typeof UpdateUserResource>;
-            const updateUserResource = new mockUpdateUserResource(mockUserResourceRepository);
-            updateUserResource.execute = jest.fn<(args:UpdateUserResourceArgsType) => Promise<boolean>>(
-                (args:UpdateUserResourceArgsType) => {
-                    if(args.userId === 1 && args.resourceId === 1){
-                        return Promise.resolve(true);
-                    }else{
-                        return Promise.resolve(false);
-                    }
-                }
-            );
-
-            const userResourceController = new UserResourceController(
-                {} as GetUserResource, updateUserResource
-            );
             const req = { params: { userId: 1, resourceId: 2 }, body: { 
                 url: "http://example.com:12345",
                 type: "WEBDAV",
@@ -133,6 +124,44 @@ describe("UserResourceController", () => {
             await userResourceController.updateResource(req as any, res as any);
             expect(res.status).toHaveBeenCalledWith(404);
             expect(res.json).toHaveBeenCalledWith({ error: "Resource not found" });
+        });
+    });
+
+    describe("createResource", () => {
+        it('should return 200 when resource is created successfully', async () => {
+            const req = { params: { userId: 1 }, body: { 
+                url: "http://example.com:12345",
+                type: "WEBDAV",
+                username: "username001",
+                password: "password001",
+                directory: "/path/to/directory001"
+            } };
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            };
+
+            await userResourceController.createResource(req as any, res as any);
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.json).toHaveBeenCalledWith({ message: 'Resource created' });
+        });
+
+        it('should return 400 when resource type is invalid', async () => {
+            const req = { params: { userId: 1 }, body: { 
+                url: "http://example.com:12345",
+                type: "INVALID_TYPE",
+                username: "username001",
+                password: "password001",
+                directory: "/path/to/directory001"
+            } };
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            };
+
+            await userResourceController.createResource(req as any, res as any);
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ error: "Invalid resource type" });
         });
     });
 });
--- packages/backend/src/interfaces/controllers/UserResourceController.ts
+++ packages/backend/src/interfaces/controllers/UserResourceController.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import GetUserResource from '../../application/userResource/GetUserResource';
 import {UpdateUserResource} from '../../application/userResource/UpdateUserResource';
+import { CreateUserResource } from '../../application/userResource/CreateUserResource';
 
 
 
@@ -8,11 +9,16 @@ class UserResourceController {
     public readonly router = express.Router({mergeParams: true});
     private getUserResource: GetUserResource;
     private updateUserResource: UpdateUserResource;
+    private createUserResource: CreateUserResource;
 
-    constructor(getUserResource: GetUserResource, updateUserResource: UpdateUserResource){
-        this.router.get('/:resourceId', this.getResource.bind(this));
+    constructor(getUserResource: GetUserResource, 
+            updateUserResource: UpdateUserResource,
+            createUserResource: CreateUserResource){
         this.getUserResource = getUserResource;
         this.updateUserResource = updateUserResource;
+        this.createUserResource = createUserResource;
+        this.router.get('/:resourceId', this.getResource.bind(this));
+        this.router.put('/:resourceId', this.updateResource.bind(this));
     }
 
     public async getResource(req: express.Request, res: express.Response){
@@ -30,7 +36,14 @@ class UserResourceController {
     public async updateResource(req: express.Request, res: express.Response){
         const resourceId = parseInt(req.params.resourceId);
         const userId = parseInt(req.params.userId);
+        
         const resource = req.body;
+        const type = resource.type;
+        if(type !== "WEBDAV"){
+            res.status(400).json({ error: 'Invalid resource type' });
+            return;
+        }
+        
         const updated = await this.updateUserResource.execute({
             userId,
             resourceId,
@@ -45,6 +58,34 @@ class UserResourceController {
             res.status(200).json({ message: 'Resource updated' });
         }else{
             res.status(404).json({ error: 'Resource not found' });
+        }
+    }
+
+    public async createResource(req: express.Request, res: express.Response){
+        const userId = parseInt(req.params.userId);
+        const resource = req.body;
+        const type = resource.type;
+        if(type !== "WEBDAV"){
+            res.status(400).json({ error: 'Invalid resource type' });
+            return;
+        }
+        const created = await this.createUserResource.execute({
+            userId,
+            url: resource.url,
+            type: resource.type,
+            username: resource.username,
+            password: resource.password,
+            directory: resource.directory
+        });
+
+        if(created){
+            //201: Created
+            res.status(201).json({ 
+                message: 'Resource created',
+                resourceId: created
+            });
+        }else{
+            res.status(500).json({ error: 'Resource creation failed' });
         }
     }
 }
