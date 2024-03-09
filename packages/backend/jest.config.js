28aa2b5b1969376d7c4b6170116f5756e0404ddc
koplec <kopec@github.com>
2024-03-09 13:34:47 +0900

    backend側のjestの動作ができなくなっていたので調整

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -1,8 +1,19 @@
-module.exports = {
-  preset: "ts-jest",
+export default {
+  preset: "ts-jest/presets/default-esm", //ESモジュール用のプリセット
+  globals: {
+    'ts-jest': {
+      useESM: true, //ESモジュールを使う
+    }
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',  // ES モジュールでの相対パスの解決
+  },
+  extensionsToTreatAsEsm: ['.ts'],  // .ts ファイルを ESM として扱う
   testEnvironment: "node", //ブラウザ側テストではなくサーバ側テスト
+  testPathIgnorePatterns: [
+    "dist/",
+  ],
   transformIgnorePatterns: [
     'node_modules/(?!(webdav)/)',  // webdavモジュールをトランスパイルの対象に含める
   ],
-};
-　
+}
